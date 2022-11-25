1cb9ff4279273f70354a30c0df29f8179f737540
Mohamed Safras <awsafras@gmail.com>
2022-11-25 20:51:09 +0530

    sign in methods are moved into it's own component

---
+++ src/utils/firebase/SignInMethods.js
@@ -0,0 +1,36 @@
+import { auth, githubProvider, googleProvider } from "./firebase.utils";
+
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signInWithRedirect,
+} from "firebase/auth";
+
+// google provider
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
+// sign in with popup
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+
+// github provider
+
+githubProvider.addScope("repo");
+githubProvider.setCustomParameters({
+  allow_signup: "false",
+});
+
+export const signInWithGithubPopup = () =>
+  signInWithPopup(auth, githubProvider);
+
+// Email auth
+
+export const signInWithEmailPassword = (email, password) =>
+  createUserWithEmailAndPassword(auth, email, password);
+
+// sign in with redirect
+
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
