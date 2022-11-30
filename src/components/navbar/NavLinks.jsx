c8876007cf205c3dbdd11b50784e85ccb4d86b63
Mohamed Safras <awsafras@gmail.com>
2022-11-30 23:49:47 +0530

    nav links moved into it's own component

---
+++ src/components/navbar/NavLinks.jsx
@@ -0,0 +1,48 @@
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import { IconButton } from "@mui/material";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../../styles/navbar/navlinks.styles.scss";
+
+const NavLinks = () => {
+  return (
+    <ul className="nav-links">
+      <li className="nav-link">
+        <NavLink
+          className={({ isActive }) => (isActive ? "link active" : "link")}
+          to="/shop"
+        >
+          SHOP
+        </NavLink>
+      </li>
+      <li className="nav-link">
+        <NavLink
+          className={({ isActive }) => (isActive ? "link active" : "link")}
+          to="/contact"
+        >
+          CONTACT
+        </NavLink>
+      </li>
+      <li className="nav-link">
+        <NavLink
+          className={({ isActive }) => (isActive ? "link active" : "link")}
+          to="/signin"
+        >
+          SIGN IN
+        </NavLink>
+      </li>
+      <li className="nav-link">
+        <NavLink
+          className={({ isActive }) => (isActive ? "link active" : "link")}
+          to="/cart"
+        >
+          <IconButton>
+            <ShoppingBagIcon color="primary" sx={{ fontSize: 25 }} />
+          </IconButton>
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default NavLinks;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,55 +1,22 @@
-import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import MenuIcon from "@mui/icons-material/Menu";
 import { IconButton } from "@mui/material";
-import React from "react";
-import { BsBagDash } from "react-icons/bs";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { ReactComponent as Crown } from "../../assets/crown.svg";
 import "../../styles/navbar/navbar.styles.scss";
+import NavLinks from "./NavLinks";
 const Navbar = () => {
   return (
-    <React.Fragment>
-      <nav className="nav-bar">
-        <Link to="/" className="logo">
-          <Crown />
-        </Link>
-        <ul className="nav-links">
-          <li className="nav-link">
-            <NavLink
-              className={({ isActive }) => (isActive ? "link active" : "link")}
-              to="/shop"
-            >
-              SHOP
-            </NavLink>
-          </li>
-          <li className="nav-link">
-            <NavLink
-              className={({ isActive }) => (isActive ? "link active" : "link")}
-              to="/contact"
-            >
-              CONTACT
-            </NavLink>
-          </li>
-          <li className="nav-link">
-            <NavLink
-              className={({ isActive }) => (isActive ? "link active" : "link")}
-              to="/signin"
-            >
-              SIGN IN
-            </NavLink>
-          </li>
-          <li className="nav-link">
-            <NavLink
-              className={({ isActive }) => (isActive ? "link active" : "link")}
-              to="/cart"
-            >
-              <IconButton>
-                <ShoppingBagIcon sx={{ color: "#fff", fontSize: 25 }} />
-              </IconButton>
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </React.Fragment>
+    <nav className="nav-bar">
+      <Link to="/" className="logo">
+        <Crown />
+      </Link>
+
+      <NavLinks />
+
+      <IconButton>
+        <MenuIcon color="primary" sx={{ fontSize: 40 }} />
+      </IconButton>
+    </nav>
   );
 };
 
