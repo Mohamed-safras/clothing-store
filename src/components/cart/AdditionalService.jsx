547d3d761a4818a9c0c3d270961b52535987cd4d
Mohamed Safras <awsafras@gmail.com>
2023-02-15 10:19:09 +0530

    additional service has been added

---
+++ src/components/cart/AdditionalService.jsx
@@ -0,0 +1,46 @@
+import CustomizedSwitches from "../Switch.component";
+import {
+  Container,
+  PackagePrice,
+  PackageType,
+  ServiceEnabalButton,
+  ServicePackage,
+} from "./additional_service,styles";
+const AdditionalService = () => {
+  return (
+    <Container>
+      <ServicePackage>
+        <PackageType>
+          <span>Care+Package</span>
+          <span>One year of additional care</span>
+        </PackageType>
+        <PackagePrice>Rs.{"2.00"}</PackagePrice>
+        <ServiceEnabalButton>
+          <CustomizedSwitches />
+        </ServiceEnabalButton>
+      </ServicePackage>
+      <ServicePackage>
+        <PackageType>
+          <span>Care+Package</span>
+          <span>One year of additional care</span>
+        </PackageType>
+        <PackagePrice>Rs.{"2.00"}</PackagePrice>
+        <ServiceEnabalButton>
+          <CustomizedSwitches />
+        </ServiceEnabalButton>
+      </ServicePackage>
+      <ServicePackage>
+        <PackageType>
+          <span>Care+Package</span>
+          <span>One year of additional care</span>
+        </PackageType>
+        <PackagePrice>Rs.{"2.00"}</PackagePrice>
+        <ServiceEnabalButton>
+          <CustomizedSwitches />
+        </ServiceEnabalButton>
+      </ServicePackage>
+    </Container>
+  );
+};
+
+export default AdditionalService;
